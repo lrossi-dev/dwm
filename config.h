e2efaacfac29ac4c9ebb8312c0fb2fd519116bcd
Leonardo Rossi <lrossi.dev@protonmail.com>
2020-08-16 10:33:59 -0300

    Applying colorbar patch

--- config.h
+++ config.h
@@ -1,28 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char bg[]              = "#090909";
+static const char fg[]              = "#efefef";
+static const char color[]           = "#f99569";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /*               fg         bg         border   */
+	[SchemeNorm] = { fg,        bg,        bg },
+	[SchemeSel]  = { color,     bg,        fg },
+	[SchemeStatus]  = { fg,     bg,        "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { color, bg,        "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]  = { fg,   bg,        "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { fg,    bg,        "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  = { fg,   bg,        "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* gaps config */
 static const int gappih = 10;
 static const int gappiv = 10;
 static const int gappov = 10;
-static const int gappoh = 1;
+static const int gappoh = 5;
 static const int smartgaps = 1;
 
 /* tagging */
@@ -63,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb",  bg, "-nf", fg, "-sb", bg, "-sf", color, "-l", "10", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
